f2adc2140c79f0215157bf76c5ba692134e96d00
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-07-04 13:23:13 +0200

    Add test

---
+++ day01/day01_test.go
@@ -0,0 +1,19 @@
+package day01
+
+import "testing"
+
+func TestCountMeasurementIncreases(t *testing.T) {
+	cases := []struct {
+		in []int
+		want int
+	}{
+		{TestData(), 7},
+		{Data(), 1553},
+	}
+	for _, c := range cases {
+		got := CountMeasurementIncreases(c.in)
+		if got != c.want {
+			t.Errorf("CountMeasurementIncreases(%d) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
