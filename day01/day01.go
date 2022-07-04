7360c7855d6726c786f40faa84881174b3d034b0
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-07-04 12:32:27 +0200

    Start with day 01

---
+++ day01/day01.go
@@ -0,0 +1,9 @@
+package day01
+
+// func CountMeasurementIncreases(measurements []int) int {
+// 	return 0
+// }
+
+func CountMeasurementIncreases() int {
+	return 2
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tiesmaster/advent-of-code-2021-go/day01"
+)
 
 func main() {
-	fmt.Println("Hello code!!!")
+	fmt.Println(day01.CountMeasurementIncreases())
 }
