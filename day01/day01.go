c7997a503c5f25e66193fe9fcf40097d28f54d2e
Thijs Brobbel <thijsbrobbel@gmail.com>
2022-07-05 09:21:55 +0200

    Implement day01.part2

--- day01/day01.go
+++ day01/day01.go
@@ -1,12 +1,25 @@
 package day01
 
 func CountMeasurementIncreases(measurements []int) int {
-
 	increases := 0
 	for i := 1; i < len(measurements); i++ {
 		if measurements[i-1] < measurements[i] {
 			increases++
 		}
+	}
+
+	return increases
+}
+
+func CountMeasurementIncreasesSlidingWindow(measurements []int) int {
+	increases := 0
+	previousSum := 0
+	for i := 0; i < len(measurements) - 3; i++ {
+		sum := measurements[i] + measurements[i+1] + measurements[i+2]
+		if previousSum < sum {
+			increases++
+		}
+		previousSum = sum
 	}
 
 	return increases
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -17,3 +17,20 @@ func TestCountMeasurementIncreases(t *testing.T) {
 		}
 	}
 }
+
+
+func TestCountMeasurementIncreasesSlidingWindow(t *testing.T) {
+	cases := []struct {
+		in   []int
+		want int
+	}{
+		{TestData, 5},
+		{Data, 1597},
+	}
+	for _, c := range cases {
+		got := CountMeasurementIncreasesSlidingWindow(c.in)
+		if got != c.want {
+			t.Errorf("CountMeasurementIncreasesSlidingWindow(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
